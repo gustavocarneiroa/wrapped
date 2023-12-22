6ab3ef3d4099169fe016a463beed1fee94b506f0
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2023-12-22 10:39:06 -0300

    adding students pages

--- src/utils/sudents.ts
+++ src/utils/sudents.ts
@@ -3,74 +3,49 @@ export const students : { [key: string]: {
     folder: string,
     coupon: string,
 }} = {
-    "f752fd75-e9db-4694-8c8a-9cf4a0b02d7e": {
-        name: "Gustavo",
-        folder: "teste",
-        coupon: "GUSTAVOC2024",
-    },
-    "0f6dde03-fc39-4df7-8a73-950f1ae83f2c": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
-    },
     "948deb8a-291d-4709-9fcb-0d811b85c537": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+        name: "Alessandro Vieira",
+        folder: "Al3554ndr0V131r4",
+        coupon: "ALESSANV2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c539": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "7a73f39f-e315-495a-aa55-6ec3866cb676": {
+        name: "Beatriz Romano",
+        folder: "B34tr1ZR0M4N0",
+        coupon: "BEATRIZR2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c530": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "27bd7d34-4553-4bc6-9ced-7c98a3f84824": {
+        name: "Demostenes Ramos Cassiano",
+        folder: "C45514n0",
+        coupon: "CASSIANO2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c531": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "95ef5a68-2253-49a9-9915-31e23cda6b7a": {
+        name: "Felipe Cabral",
+        folder: "F3l1p3C4br4l",
+        coupon: "FELIPECB2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c532": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "ae52e2d0-4912-4946-9cef-14b580b3021c": {
+        name: "Alex da Rocha",
+        folder: "AL3xD4R0CH4",
+        coupon: "ALEXDROC2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c533": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "24816608-6297-4b3d-9812-f870b460d891": {
+        name: "Ingryd Marques",
+        folder: "INGRYDM4RQU3s",
+        coupon: "INGRYDMQ2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c534": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "dd4abe1c-6a8b-4708-930c-2d5abaac9aec": {
+        name: "Nathalia Marques",
+        folder: "N4TH4L14M4rqu35",
+        coupon: "NATHALMQ2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c535": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "ca0ffbee-464b-46bb-9e29-937ddf1f9fc7": {
+        name: "Hugo Holanda",
+        folder: "HUG0H0l4Nd4",
+        coupon: "HUGOHOLN2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c536": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
+    "20b52bff-9d88-436f-96b0-e86580efad8c": {
+        name: "Bianca Oliveira",
+        folder: "b14nc40l1v31r4",
+        coupon: "BIANCAOL2024",
     },
-    "948deb8a-291d-4709-9fcb-0d811b85c538": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
-    },
-    "948deb8a-291d-4709-9fcb-0d811b85c529": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
-    },
-    "948deb8a-291d-4709-9fcb-0d811b85c528": {
-        name: "Késsia",
-        folder: "kessia",
-        coupon: "KESSIALI2024",
-    }
 };
